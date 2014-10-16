a3f851802444d007a7d5f198029364c3d9d73f70
Andrea Stagi <stagi.andrea@gmail.com>
2014-10-16 13:16:31 +0200

    Buildslist server now read config file

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 import os.path
+from master import config
 from flask import Flask
 from flask.ext.autoindex import AutoIndex
 
@@ -6,4 +7,4 @@ app = Flask(__name__)
 AutoIndex(app, 'builds', add_url_rules=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=config.BUILDSLIST_PORT)
